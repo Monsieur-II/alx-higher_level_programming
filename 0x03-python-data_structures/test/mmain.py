27d7b27a0e0418711145e9f0aa59d92c1206fc85
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-06-11 11:43:05 -0700

    switchh

--- 0x03-python-data_structures/test/mmain.py
+++ 0x03-python-data_structures/test/mmain.py
@@ -19,6 +19,6 @@ delete_at = __import__('11-delete_at').delete_at
 
 my_list = [1, 2, 3, 4, 5]
 idx = 3
-new_list = delete_at(my_list, idx)
+new_list = delete_at(my_list)
 print(new_list)
 print(my_list)
