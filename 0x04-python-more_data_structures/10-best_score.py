49f84cdb33674696593bd036aa6a0adf974cec9f
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-06-19 09:33:17 -0700

    add

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    """ Returns key with biggest integer value"""
+    if not a_dictionary:
+        return None
+    max_key = None
+    max_value = float('-inf')
+    for key, value in a_dictionary.items():
+        if value > max_value:
+            max_value = value
+            max_key = key
+    return max_key
---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    total_weight = 0
+    total_score = 0
+
+    for i in my_list:
+        total_weight += i[1]
+        total_score += (i[0] * i[1])
+
+    average = total_score / total_weight
+    return average
