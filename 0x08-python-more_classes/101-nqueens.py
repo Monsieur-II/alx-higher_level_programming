170d1c6017a6006767a4e9d63a93f270665d4c32
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-07-04 08:46:57 -0700

    nqueens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -41,17 +41,17 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) != 2:
-        print("Usage: nqueens N", file=sys.stderr)
+        print("Usage: nqueens N")
         sys.exit(1)
 
     N = sys.argv[1]
     if not N.isdigit():
-        print("N must be a number", file=sys.stderr)
+        print("N must be a number")
         sys.exit(1)
 
     N = int(N)
     if N < 4:
-        print("N must be at least 4", file=sys.stderr)
+        print("N must be at least 4")
         sys.exit(1)
 
     Chess.nQueens(N)
