35cfe88dcd299ff8f8425ac1c14bdfefba1c6e37
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-07-08 05:58:21 -0700

    this

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """Defines an integer addition fucntion"""
 
 
@@ -10,7 +9,8 @@ def add_integer(a, b=98):
             a (int, float): First operand
             b (int, float): Second operand (optional)
 
-    Returns: the sum of a and b
+    Returns:
+             the sum of a and b
     """
     if not(isinstance(a, (int, float))):
         raise TypeError("a must be an integer")
