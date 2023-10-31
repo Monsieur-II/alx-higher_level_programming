dce9c99e9ce576cf86654eb829e92babffd78a44
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-10-31 00:38:52 +0000

    pycodestyle

--- 0x08-python-more_classes/my_101-nqueens.py
+++ 0x08-python-more_classes/my_101-nqueens.py
@@ -12,14 +12,18 @@ class Chess:
         board = [[] for i in range(n)]
 
         def backtrack(r):
+            # we will only get to this point if all rows (row 0 to row n - 1) found a free spot
             if r == n:
                 copy = board.copy()
                 print(copy)
                 return
 
             for c in range(n):
-                if c in columns or r + c in posConsts or r - c in negConsts:
+                if c in columns or (r + c) in posConsts or (r - c) in negConsts:
                     continue
+
+            # if the current column or the diagonal constants of current position are not taken
+            # fill it else, move to next column
 
                 columns.add(c)
                 posConsts.add(r + c)
@@ -27,7 +31,14 @@ class Chess:
                 board[r].append(r)
                 board[r].append(c)
 
+                # after filling perform an iteration for the next ROW
+                # If the next row finds a free space it will take it and call the next row
+
                 backtrack(r + 1)
+
+                # At this point, all possible child solutions under this position,(r, c) have been made
+                # so we clear the filled position, after removing column and diagonal constants from
+                # respective sets{}. Then move to the next column and repeat.
 
                 columns.remove(c)
                 posConsts.remove(r + c)
