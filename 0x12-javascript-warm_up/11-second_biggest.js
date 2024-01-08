f9a75894a2bfd42c3041ef7af9b837955e4f4c2a
Evans Nyarko <evans.nyarko39@yahoo.com>
2024-01-08 12:50:53 +0000

    bug fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-
 if (process.argv.length <= 3) {
   console.log('0');
 } else {
   const args = process.argv.slice(2).map(Number);
-
   args.sort().reverse();
   console.log(args[1]);
 }
