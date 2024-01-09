8bd6e02a36d779ff91cc19687908a6d7bf89d92c
Evans Nyarko <evans.nyarko39@yahoo.com>
2024-01-09 07:49:23 +0000

    js sort

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ if (process.argv.length <= 3) {
   console.log('0');
 } else {
   const args = process.argv.slice(2).map(Number);
-  args.sort().reverse();
+  args.sort(function (a, b) { return b - a; });
   console.log(args[1]);
 }
