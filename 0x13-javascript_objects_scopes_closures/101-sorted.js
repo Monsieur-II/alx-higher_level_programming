69f556653dd2f271849fe39390c9816af35c3378
Evans Nyarko <evans.nyarko39@yahoo.com>
2024-01-09 10:58:36 +0000

    0x13

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const dic = require('./101-data').dict;
+
+const keys = Object.keys(dic);
+const values = new Set(Object.values(dic));
+
+let arr;
+const res = {};
+for (const value of values) {
+  arr = [];
+  for (const k of keys) {
+    if (dic[k] == value) {
+      arr.push(k);
+    } 
+  }
+  res[value] = arr;
+}
+console.log(res);
+
---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  const arr = [];
+  let length = list.length - 1;
+
+  while (length >= 0) {
+    arr.push(list[length]);
+    length--;
+  }
+
+  return arr;
+}
