5f5ac4357a0a6bef4919850e439ae80b77603257
Evans Nyarko <evans.nyarko39@yahoo.com>
2024-02-21 08:38:32 +0000

    5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,23 +1,4 @@
 #!/usr/bin/node
-
-const request = require('request');
 const fs = require('fs');
-
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error('Status code:', response.statusCode);
-  } else {
-    fs.writeFile(filePath, body, 'utf8', (error) => {
-      if (error) {
-        console.error('Error writing file:', error);
-      } else {
-        console.log('File saved successfully!');
-      }
-    });
-  }
-});
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
